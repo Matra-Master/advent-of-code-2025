90b57e58f5af9eb9f779340d6c18e06da55ca50b
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-12-01 18:30:41 -0300

    Started something on part1

--- 01/part1.lua
+++ 01/part1.lua
@@ -1,1 +1,20 @@
 require 'utils'
+
+local input, err = io.open('input', 'r')
+
+if not input then
+  print ("Error: Couldn't open input file. Reason: " .. (err or "Unknown Error"))
+  return
+end
+
+local message = input:read("*all")
+
+print (message)
+
+Line = 50
+
+function dial(--[[string]] line)
+  print (line[1])
+end
+
+dial('R60')
