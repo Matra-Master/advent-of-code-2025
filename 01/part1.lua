c34a3119425c7523779200b692a7c0505b6ecdef
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-12-01 22:50:30 -0300

    Solved part 1!

--- 01/part1.lua
+++ 01/part1.lua
@@ -1,20 +1,25 @@
 require 'utils'
 
 local input, err = io.open('input', 'r')
-
 if not input then
   print ("Error: Couldn't open input file. Reason: " .. (err or "Unknown Error"))
   return
 end
 
-local message = input:read("*all")
+local result = 0
+while true do
+  Line = input:read()
+  if Line == nil then break end
 
-print (message)
-
-Line = 50
-
-function dial(--[[string]] line)
-  print (line[1])
+  if Partial == nil then
+    Partial = Dial(50, Line)
+  else
+    Partial = Dial(Partial, Line)
+  end
+  if Partial == 0 then
+    result = result + 1
+  end
+  print("Partial: " .. Partial .. "\n")
 end
 
-dial('R60')
+print("Result is: ".. result)
--- 01/utils.lua
+++ 01/utils.lua
@@ -1,1 +1,18 @@
 -- Common functions and variables
+
+local function circularMath(number)
+  return math.fmod(math.fmod(number, 100) + 100, 100)
+end
+
+function Dial(pos, line)
+  local letter = string.match(line, "%a")
+  local number = string.match(line, "%d+")
+
+  if letter == "R" then
+    return circularMath(pos + number)
+  end
+  if letter == "L" then
+    return circularMath(pos - number)
+  end
+
+end
